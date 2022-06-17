1972aef41a7e3381c53bb9e9828a7de7b73a20ad
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-17 11:29:50 +0300

    commit tree 6fa56007f5941cee97a4e9d8c358a9732dd7b12d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ name, email }),
+    };
+  
+    let request = fetch("http://localhost:3000/users", configurationObject)
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => {
+        document.getElementById("error").textContent = error.message;
+      });
+  
+    return request;
+  }
+  
